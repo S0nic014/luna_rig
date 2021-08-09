84b87a4523217a354d1b7ce0e8ca83560be05585
Dmitrii Shevchenko <47799401+S0nic014@users.noreply.github.com>
2021-08-09 16:32:12 +0100

    Cleanup PyBuild imports

--- core/pybuild.py
+++ core/pybuild.py
@@ -1,40 +1,29 @@
 import pymel.core as pm
 import timeit
-from PySide2 import QtCore
 
 from luna import Logger
-from luna import Config
-from luna import BuildVars
-from luna.utils import maya_utils
-from luna.workspace import project
-from luna.workspace import asset
+import luna.workspace
 import luna_rig
-from luna_rig.functions import asset_files
-
-
-class _buildSignals(QtCore.QObject):
-    started = QtCore.Signal()
-    done = QtCore.Signal()
+import luna.utils.maya_utils as maya_utils
+import luna_rig.functions.asset_files as asset_files
 
 
 class PyBuild(object):
     def __init__(self, asset_type, asset_name, existing_character=None):
-        self.signals = _buildSignals()
 
         # Get project instance
-        self.project = project.Project.get()  # type: project.Project
+        self.project = luna.workspace.Project.get()
         if not self.project:
             pm.warning("Project is not set!")
             return
 
         # Start build
         pm.scriptEditorInfo(e=1, sr=1)
-        self.signals.started.emit()
         pm.newFile(f=1)
         self.start_time = timeit.default_timer()
         Logger.info("Initiating new build...")
 
-        self.asset = asset.Asset(self.project, asset_name, asset_type)
+        self.asset = luna.workspace.Asset(self.project, asset_name, asset_type)
         # Import model and componets files
         asset_files.import_model()
         asset_files.import_skeleton()
@@ -58,7 +47,6 @@ class PyBuild(object):
         self.character.geometry_grp.overrideColor.set(1)
 
         # Report completion
-        self.signals.done.emit()
         Logger.info("Build finished in {0:.2f}s".format(timeit.default_timer() - self.start_time))
         pm.scriptEditorInfo(e=1, sr=0)
 
